4d5e65c38ace24f9e0e714fc74daf1996c04f2d2
Brent Bauman <brentbauman86@gmail.com>
2018-09-28 23:10:10 +0000

    added sign up and log in links to home page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,11 +6,21 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
     enable :sessions
-    enable :session_secret, "shushy_shush"
+    set :session_secret, "shushy_shush"
   end
 
   get "/" do
     erb :welcome
   end
 
+  helpers do
+
+    def logged_in?
+      !!current_user
+    end
+
+    def current_user
+      @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+    end
+  end
 end
