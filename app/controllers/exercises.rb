01ce07c00ef90957849532336383480a9c2ad43c
Brent Bauman <brentbauman86@gmail.com>
2018-09-29 01:43:23 +0000

    build exercise edit form and route

--- app/controllers/exercises.rb
+++ app/controllers/exercises.rb
@@ -4,4 +4,22 @@ class ExercisesController < ApplicationController
     @exercises = Exercise.all
     erb :'exercises/index'
   end
+
+
+
+
+  get "/exercises/:id/edit" do
+    @exercises = Exercise.find(params[:id])
+    erb :'exercises/edit'
+  end
+
+  post "/exercises/:id" do
+    redirect_if_not_logged_in
+    @exercises = Exercise.find(params[:id])
+    unless Exercise.valid_params?(params)
+      redirect "/exercises/#{@exercises.id}"
+    end
+    @exercises.update(params[:exercise_id])
+    redirect "/exercises/#{@exercises.id}"
+  end
 end
