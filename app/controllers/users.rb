f641fe5395e642ceaf08e12eb15fbec86944dd25
Brent Bauman <brentbauman86@gmail.com>
2018-09-29 00:47:14 +0000

    added signup route and create_user view

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -1,6 +1,23 @@
 class UsersController < ApplicationController
 
   get '/signup' do
-    
+    hey
+    # if !logged_in?
+    #   erb :"users/create_user"
+    # else
+    #   redirect to '/exercises'
+    # end
   end
-end
+
+  post '/singup' do
+    if params[:user].empty?
+      redirect to '/signup'
+    else
+      @user = User.new(params[:user])
+      @user.save
+        session[:user_id] = @user.id
+
+        redirect to '/exercises'
+      end
+    end
+  end
