9907d017c647a5d6c7f8da9f50c641036b13f76f
Brent Bauman <brentbauman86@gmail.com>
2018-10-02 18:44:47 +0000

    start making users show page

--- app/controllers/exercises.rb
+++ app/controllers/exercises.rb
@@ -31,25 +31,29 @@ end
   end
 
   get "/exercises/:id/edit" do
-    @exercises = Exercise.find_by(id: params[:id])
+    binding.pry
+    @exercise = Exercise.find_by(id: params[:id])
 
-    if current_user == @exercises
+    if current_user.id == @exercise.user_id
       erb :'/exercises/edit'
     else
       redirect to '/exercises'
   end
 end
 
-  # patch "/exercises/:id" do
-  #   @exercises = Exercise.find_by(id: params[:id])
-  #   if current_user != @exercises.user_id
-  #
-  #   if @exercises.update(name: params[:name], muscle_group: params[:muscle_group], rep_range: params[:rep_range])
-  #     flash[:message] = "Exercise was successfully updated"
-  #     redirect to "/exercises/#{@exercises.id}"
-  #   else
-  #     erb :"/exercises/edit"
-  #   end
-  # end
+  patch "/exercises/:id" do
+    binding.pry
+    @exercises = Exercise.find_by(id: params[:id])
+    redirect '/exercises'
+    if current_user != @exercises.user_id
+
+    if @exercises.update(name: params[:name], muscle_group: params[:muscle_group], rep_range: params[:rep_range])
+      flash[:message] = "Exercise was successfully updated"
+      redirect to "/exercises/#{@exercises.id}"
+    else
+      erb :"/exercises/edit"
+    end
+  end
+end
 end
 end
